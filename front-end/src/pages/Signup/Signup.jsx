a58ffe63d84d946b4886495cfb387fd1cbdce3e2
Aboubakar2004 <aboubakarbamba718@gmail.com>
2025-09-15 23:43:40 +0200

    signup page creation

--- front-end/src/pages/Signup/Signup.jsx
+++ front-end/src/pages/Signup/Signup.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "../Signup/Signup.css";
 
-function Signup() {
+function SignUp() {
   return (
-    <div>
-      <h1 className="inscription-title">Ceci est la page Inscription</h1>
+    <div className="sign-up-page-container">
+      <div className="sign-up-page-content">
+        <div className="sign-up-title">
+          <h1>Inscription</h1>
+        </div>
+        <div className="email">
+          Email
+          <input type="email" className="email-input" />
+        </div>
+        <div className="password">
+          Mots de passe
+          <input type="password" className="password-input" />
+        </div>
+        <button className="sign-up-page-button">S'inscrire</button>
+      </div>
     </div>
   );
 }
 
-export default Signup;
+export default SignUp;
