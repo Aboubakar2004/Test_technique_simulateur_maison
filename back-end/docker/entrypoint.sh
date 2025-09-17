f25a5bd022fca5d44be72e4165e8c81408ec77f9
Aboubakar2004 <aboubakarbamba718@gmail.com>
2025-09-17 23:40:47 +0200

    docker update

--- back-end/docker/entrypoint.sh
+++ back-end/docker/entrypoint.sh
@@ -21,7 +21,7 @@ php artisan key:generate --force
 
 # Exécuter les migrations
 echo "Exécution des migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 # Nettoyer le cache
 echo "Nettoyage du cache..."
